7c1bbce8be672ea4c37fbb31314312c99a06bea0
Patrick <skovskiver@gmail.com>
2021-02-25 14:26:14 +0100

    Færdiggjort astronaut opgave

--- src/main/java/com/example/demo/controllers/AstronautsInSpaceController.java
+++ src/main/java/com/example/demo/controllers/AstronautsInSpaceController.java
@@ -4,6 +4,7 @@ import com.example.demo.models.Astronaut;
 import com.example.demo.models.AstronautsInSpace;
 import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //import com.google.gson.Gson;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 @Controller
 public class AstronautsInSpaceController {
     public AstronautsInSpace astronautsInSpaceData;
+    public Astronaut astronautData;
 
     public AstronautsInSpaceController() throws IOException {
         URL astronautsURL= new URL("http://api.open-notify.org/astros.json");
@@ -27,17 +29,22 @@ public class AstronautsInSpaceController {
     }
 
     @GetMapping("/")
-    @ResponseBody
-    public String renderAstronauts() throws IOException {
+    public String renderAstronauts(Model model) throws IOException {
 
         Integer numberOfPeopleinSpace = this.astronautsInSpaceData.number;
 
         // Investigate what properties can be found on the individual Astronaut
         ArrayList<Astronaut> astronautsInSpace = this.astronautsInSpaceData.people;
 
+        model.addAttribute("astronautsNumber", numberOfPeopleinSpace);
+
+        model.addAttribute("astronauts", astronautsInSpace);
+
+
+
         System.out.println(numberOfPeopleinSpace);
         System.out.println(astronautsInSpace);
 
-        return "stuff";
+        return "index.html";
     }
 }
--- src/main/java/com/example/demo/models/Astronaut.java
+++ src/main/java/com/example/demo/models/Astronaut.java
@@ -3,4 +3,9 @@ package com.example.demo.models;
 public class Astronaut {
     public String craft;
     public String name;
+
+    @Override
+    public String toString (){
+        return "Astronaut: > Name: " + name + " | Craft: " + craft;
+    }
 }
